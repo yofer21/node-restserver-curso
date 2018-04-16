b5c61390c843172f1b4030185f68b6c0f1cd3341
Fernando <yofer21@gmail.com>
2018-04-15 22:06:43 -0300

    RestServer - Usuario

--- server/server.js
+++ server/server.js
@@ -1,47 +1,25 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser')
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/usuario', function (req, res) {
-  res.json('get Usuario');
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function (req, res) {
 
-  let body = req.body;
+mongoose.connect(process.env.URLDB, (err, res) => {
+  if (err) throw err;
 
-  if (!body.nombre) {
-    res.status(400).json({
-      ok: false,
-      mensaje: 'El nombre es necesario'
-    });
-  } else {
-    res.json({
-      persona: body
-    });
+  console.log('Base de datos ONLINE');
 
-  }
-
-});
-
-app.put('/usuario/:id', function (req, res) {
-
-  let id = req.params.id;
-
-  res.json({
-    id
-  });
-});
-app.delete('/usuario', function (req, res) {
-  res.json('delete Usuario');
 });
 
 app.listen(process.env.PORT, () => {
