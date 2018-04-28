ed9b64235de581edc740447c1b236c7a649a0faa
Fernando <yofer21@gmail.com>
2018-04-28 19:04:39 -0300

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -45,7 +45,32 @@ let verificaAdmin_Role = (req, res, next) => {
 
 };
 
+// =======================================
+//  Verifica token para imagen
+// =======================================
+let verificaTokenImg = (req, res, next) => {
+  let token = req.query.token;
+
+  jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+    if (err) {
+      return res.status(401).json({
+        ok: false,
+        mensaje: 'Token no valido',
+        err
+      });
+    }
+
+    req.usuario = decoded.usuario;
+
+    next();
+
+  });
+
+}
+
 module.exports = {
   verificaToken,
-  verificaAdmin_Role
+  verificaAdmin_Role,
+  verificaTokenImg
 }
